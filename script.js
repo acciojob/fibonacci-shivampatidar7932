6d3630517bfed434268647e4c6dc25922d5ce2c7
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-11-20 10:04:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
-// your code here
+    if (num <= 0) {
+        console.log("Invalid input. Please provide a positive integer.");
+        return -1; // or throw an exception for invalid input
+    }
+
+    // Initialize the first two Fibonacci numbers
+    let fibSequence = [0, 1];
+
+    // Generate the Fibonacci sequence up to the num-th term
+    for (let i = 2; i <= num; i++) {
+        fibSequence.push(fibSequence[i - 1] + fibSequence[i - 2]);
+    }
+
+    // Return the num-th Fibonacci number
+    return fibSequence[num];
 }
 
 module.exports = fibonacci;
