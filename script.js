18771f75dfeab58b38bf615652376faa853d497c
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-11-23 11:39:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-const fibonacci = (num) => {
-  // (Same fibonacci function code as before)
-
-  if (num <= 0) {
-    return "Invalid input. Please provide a positive integer.";
-  } else if (num === 1) {
-    return 0;
-  } else if (num === 2) {
-    return 1;
-  }
-
-  let prev = 0;
-  let current = 1;
-
-  for (let i = 3; i <= num; i++) {
-    const next = prev + current;
-    prev = current;
-    current = next;
-  }
-
-  return current;
-};
-
-let userInput = prompt("Enter a positive integer:");
-userInput = parseInt(userInput, 10);
-
-if (isNaN(userInput) || userInput <= 0) {
-  console.log("Invalid input. Please provide a positive integer.");
-} else {
-  const result = fibonacci(userInput);
-  console.log(`The ${userInput}-th Fibonacci number is: ${result}`);
+function fibonacci(num) {
+    if (num <= 0) {
+        return "Input should be a positive integer greater than 0";
+    } else if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    } else {
+        let a = 0, b = 1;
+        for (let i = 3; i <= num; i++) {
+            let temp = b;
+            b = a + b;
+            a = temp;
+        }
+        return b;
+    }
 }
 
-module.exports = fibonacci;
+// Take user input
+let userInput = prompt("Enter a positive integer: ");
+let num = parseInt(userInput);
+
+// Check if the input is a valid positive integer
+if (Number.isNaN(num) || num <= 0) {
+    alert("Invalid input. Please enter a positive integer greater than 0.");
+} else {
+    // Call the fibonacci function and display the result using alert
+    alert(`The ${num}th Fibonacci number is: ${fibonacci(num)}`);
+}
